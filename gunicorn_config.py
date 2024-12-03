2ad0c67ce213512d74fa7c13c81463627ab925eb
javier321jg <103332173+javier321jg@users.noreply.github.com>
2024-12-03 04:53:21 -0500

    Update gunicorn_config.py
    sa

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,8 +1,9 @@
 # gunicorn_config.py
-timeout = 300  # aumentar a 5 minutos
-workers = 1    # reducir a 1 worker para evitar problemas de memoria
+timeout = 300
+workers = 1
 worker_class = 'gthread'
 threads = 4
-preload_app = True  # pre-cargar la aplicación
+preload_app = True
 max_requests = 1000
 max_requests_jitter = 50
+bind = "0.0.0.0:$PORT"
